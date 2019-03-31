3dbf30ea216e10311f20b84f587923e61c571bf0
aporquet <antoine.porquet34070@gmail.com>
2019-03-31 18:39:20 +0200

    clean up Skills Management

--- src/main/java/model/Recruiter.java
+++ src/main/java/model/Recruiter.java
@@ -14,37 +14,4 @@ class Recruiter {
         return skills.getKeySkills().containsAll(candidate.getKeySkills()) && experienceYears > candidate.getExperienceYears();
     }
 
-    /*
-    public boolean isTheOnlyRecruiterWithAvailableSkills(Candidate candidate, List<Recruiter> recruiters){
-        for (Recruiter recruiter: recruiters){
-            if (recruiter.canTest(candidate)){
-                return false;
-            }
-        }
-        return true;
-    }
-
-
-    public static Recruiter getTheRecruiterWithMostSkillsMatching(Candidate candidate, List<Recruiter> recruiters){
-        int topCountMatchSkills = 0;
-        List<String> skills = new ArrayList<String>();
-        Recruiter recruiterMostAppropriate = new Recruiter(skills);
-        for (Recruiter recruiter: recruiters) {
-            if (recruiter.canTest(candidate)) {
-                int countMatchSkills = 0;
-                for (String skill : candidate.getSkills()) {
-                    if (recruiter.skills.contains(skill)) {
-                        countMatchSkills++;
-                    }
-                }
-                if (countMatchSkills > topCountMatchSkills) {
-                    topCountMatchSkills = countMatchSkills;
-                    recruiterMostAppropriate = recruiter;
-                }
-            }
-        }
-        return recruiterMostAppropriate;
-    }
-    */
-
 }
