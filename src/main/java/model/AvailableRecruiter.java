998d52e1cfd67466f9796bbf0b12e22a87bd7669
aporquet <antoine.porquet34070@gmail.com>
2019-04-06 15:36:41 +0200

    start agregate root

---
+++ src/main/java/model/AvailableRecruiter.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.List;
+
+public class AvailableRecruiter {
+
+    private List<AvailableRecruiter> recruiters;
+
+    public AvailableRecruiter(List<AvailableRecruiter> recruiters){
+        this.recruiters = recruiters;
+    }
+}
---
+++ src/test/java/model/AvailableRecruiterShould.java
@@ -0,0 +1,18 @@
+package model;
+
+import model.availability.Candidate;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AvailableRecruiterShould {
+
+    @Test
+    public void return_recruiters_who_can_test_candidate(){
+        
+
+    }
+}
