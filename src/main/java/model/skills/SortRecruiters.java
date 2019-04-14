285c7c52b21b413dcc5c945d39d6b1204f90e221
aporquet <antoine.porquet34070@gmail.com>
2019-04-14 17:47:26 +0200

    clean up

--- src/main/java/model/skills/SortRecruiters.java
+++ src/main/java/model/skills/SortRecruiters.java
@@ -8,18 +8,18 @@ import static java.util.stream.Collectors.toMap;
 
 class SortRecruiters {
 
-    private Map<RecruiterDto, Integer> unSotedRecruiters;
+    private Map<RecruiterDto, Integer> unSortedRecruiters;
 
-    public SortRecruiters (Map<RecruiterDto, Integer> unSotedRecruiters){
-        this.unSotedRecruiters = unSotedRecruiters;
+    public SortRecruiters (Map<RecruiterDto, Integer> unSortedRecruiters){
+        this.unSortedRecruiters = unSortedRecruiters;
     }
 
     Set<RecruiterDto> sort (){
-        Map<RecruiterDto, Integer> collect = unSotedRecruiters
+        Map<RecruiterDto, Integer> collect = unSortedRecruiters
                 .entrySet()
                 .stream()
                 .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-                .collect(toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2, LinkedHashMap::new));
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
         return collect.keySet();
     }
 
