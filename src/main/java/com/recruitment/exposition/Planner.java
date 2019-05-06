f48cc4b3870dc1eef8742a36883ccde53a4ea847
aporquet <antoine.porquet34070@gmail.com>
2019-05-06 14:28:49 +0200

    next step api (schedule interview)

--- src/main/java/com/recruitment/exposition/Planner.java
+++ src/main/java/com/recruitment/exposition/Planner.java
@@ -1,19 +1,36 @@
 package com.recruitment.exposition;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.sun.xml.internal.bind.v2.TODO;
+import org.springframework.web.bind.annotation.*;
+import use_case.ScheduleInterview;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 public class Planner {
 
-    @RequestMapping(value = "/Interviews", method = RequestMethod.GET)
+    private final ScheduleInterview repository;
+
+    Planner(ScheduleInterview repository) {
+        this.repository = repository;
+    }
+
+    @RequestMapping(value = "/Interviews", method = RequestMethod.POST)
     public String listInterviews(){
         return "Interviews list";
     }
 
-    @RequestMapping(value = "/testSpring", method = RequestMethod.GET)
-    public String test(){
-        return "Run !!";
+    @PostMapping("/ScheduleInterview")
+    public ScheduleInterview interview (@RequestParam int idCandidat, LocalDateTime date){
+        if(idCandidat <= 0){
+            // TODO: create Error Common Class -> idCandidat is not valid
+        }
+        if(date.compareTo(LocalDateTime.now()) < 0){
+            // TODO: create Error Common Class -> interview date is anterior than current date
+        }
     }
+
+
+
 }
