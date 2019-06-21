a552f958b2eef7758ac7256efd913a3d83537c77
aporquet <antoine.porquet34070@gmail.com>
2019-06-21 11:24:30 +0200

    spring security update

--- src/main/java/com/recruitment/config/WebSecurityConfig.java
+++ src/main/java/com/recruitment/config/WebSecurityConfig.java
@@ -1,14 +1,30 @@
 package com.recruitment.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Bean
+    @SuppressWarnings("deprecation") // For demo purpose
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() throws Exception {
+        return new InMemoryUserDetailsManager(
+                User.withDefaultPasswordEncoder()
+                        .username("test-user")
+                        .password(System.getenv("SPRING_SECURITY_USER_PASSWORD"))
+                        .roles("ADMIN", "USER")
+                        .build());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
